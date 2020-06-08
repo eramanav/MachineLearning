07a3e6abdf04eecaaca2c0d660fb0e362fc4421b
Era <era@Rams-MacBook-Air.local>
2020-06-07 20:31:31 -0700

    model prediction

--- v1.0/loadData.py
+++ v1.0/loadData.py
@@ -14,4 +14,14 @@ dataset.hist()
 scatter_matrix(dataset)
 pyplot.show()
 
+#creating model
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
 
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
